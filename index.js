c288bc469cbec121ac858543710d1919a9213d2f
Ben Yoon <BYoon12@gmail.com>
2023-06-10 14:21:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Require needed modules
+const express = require('express')
+
+// Initialize the app object
+const app = express()
+
+// Create a homepage route.
+app.get('/', function (req,res) {
+    // This gets sent to the client
+    // (your web browswer most likely!)
+    res.send('Hello World')
+})
+
+// Create a second route.
+app.get('/second', function (req, res) {
+    res.send('My Second Page!')
+})
+
+// Listen for connections
+app.listen(3000)
